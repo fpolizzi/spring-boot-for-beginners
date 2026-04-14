1e27314cebdc336158757bef7f6ac4ed336f89dd
Franco Polizzi <me@fpolizzi.com>
2026-04-14 02:36:40 +0200

    feat: add class SoftwareEngineer

---
+++ src/main/java/com/fpolizzi/SoftwareEngineer.java
@@ -0,0 +1,62 @@
+package com.fpolizzi;
+
+import java.util.Objects;
+
+/**
+ * Created by fpolizzi on 14.04.26
+ */
+public class SoftwareEngineer {
+
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id,
+                            String name,
+                            String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public Integer id() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String techStack() {
+        return techStack;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
