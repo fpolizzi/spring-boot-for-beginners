9c90fa254df743b9de6e06a7d72271359058de6d
Franco Polizzi <me@fpolizzi.com>
2026-04-14 02:10:45 +0200

    feat: add class Application

---
+++ src/main/java/com/fpolizzi/Application.java
@@ -0,0 +1,16 @@
+package com.fpolizzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fpolizzi on 14.04.26
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
