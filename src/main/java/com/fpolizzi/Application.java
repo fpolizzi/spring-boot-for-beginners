6fdc4995f211113a689688127b6e1d019e10e410
Franco Polizzi <me@fpolizzi.com>
2026-04-14 02:26:06 +0200

    feat: add first REST endpoint

--- src/main/java/com/fpolizzi/Application.java
+++ src/main/java/com/fpolizzi/Application.java
@@ -2,15 +2,24 @@ package com.fpolizzi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by fpolizzi on 14.04.26
  */
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld() {
+
+        return "Hello World Spring Boot";
     }
 }
