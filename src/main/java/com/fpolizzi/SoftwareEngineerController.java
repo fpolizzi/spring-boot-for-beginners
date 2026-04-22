7daa6d00d7f078663b53dacc020df17ca9d48537
Franco Polizzi <me@fpolizzi.com>
2026-04-23 00:06:14 +0200

    feat: add getEngineerById

--- src/main/java/com/fpolizzi/SoftwareEngineerController.java
+++ src/main/java/com/fpolizzi/SoftwareEngineerController.java
@@ -23,6 +23,14 @@ public class SoftwareEngineerController {
         return softwareEngineerService.getAllSoftwareEngineers();
     }
 
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(
+            @PathVariable Integer id
+    ) {
+
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
     @PostMapping
     public void addNewSoftwareEngineer(
             @RequestBody SoftwareEngineer softwareEngineer) {
