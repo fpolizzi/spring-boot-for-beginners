bb637071cb517d081899cc33713065812a2eccbf
Franco Polizzi <me@fpolizzi.com>
2026-04-22 02:12:56 +0200

    feat: add generatedValue for id

--- src/main/java/com/fpolizzi/SoftwareEngineer.java
+++ src/main/java/com/fpolizzi/SoftwareEngineer.java
@@ -1,6 +1,8 @@
 package com.fpolizzi;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Objects;
@@ -12,6 +14,7 @@ import java.util.Objects;
 public class SoftwareEngineer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String techStack;
